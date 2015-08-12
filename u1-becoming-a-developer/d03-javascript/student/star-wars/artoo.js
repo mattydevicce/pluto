857fd8a353aaf392135f4f56dfcb7ade215f14a1
Matt Morrisey <mattydevicce@gmail.com>
2015-08-12 19:45:57 -0400

    Touched some files for final draft

--- u1-becoming-a-developer/d03-javascript/student/star-wars/artoo.js
+++ u1-becoming-a-developer/d03-javascript/student/star-wars/artoo.js
@@ -9,19 +9,21 @@ function factorial (x){
     return x * factorial((x-1));
   }
 }
+
+
 function r2Calculator(){
   var operator, operand1, operand2, solution;
   while ( operator !== 'q') {
     
-    operator = prompt("Please enter which operator you would like to use ('+', '-', '*', '/', '**' \
-factorial). Enter 'q' to exit.");
+    operator = prompt("Please enter which operator you would like to use \
+('+', '-', '*', '/', '**', sqrt, or factorial). Enter 'q' to exit.");
     if (operator === 'q'){
       break;
     } else if (operator === 'factorial') {
-      operand1 = prompt("Please enter the number you want the factorial of.");
+      operand1 = parseInt(prompt("Please enter the number you want the factorial of."));
     } else {
-      operand1 = prompt("Please enter the first operand.");
-      operand2 = prompt("Please enter the second operand.");
+      operand1 = parseInt(prompt("Please enter the first operand."));
+      operand2 = parseInt(prompt("Please enter the second operand."));
     }
 
     if (operator === '+') {
@@ -36,11 +38,15 @@ factorial). Enter 'q' to exit.");
     } else if (operator === '/') {
       solution = operand1 / operand2;
       alert("The solution to " + operand1 + operator + operand2 + " = " + solution);
-    } else if (opertor === '**') {
+    } else if (operator === '**') {
       solution = Math.pow(operand1, operand2);
       alert("The solution to " + operand1 + operator + operand2 + " = " + solution);
+    } else if (operator === 'sqrt') {
+      solution = Math.sqrt(operand1, operand2);
+      alert("The solution to " + operand1 + operator + operand2 + " = " + solution);      
     } else if (operator === 'factorial'){
-      factorial(operand1);
+      solution = factorial(operand1);
+      alert(solution);
     } else {
       alert('(whheeaaarrrrr! beee-ooohhh) You did not enter a viable operator!')
     }
