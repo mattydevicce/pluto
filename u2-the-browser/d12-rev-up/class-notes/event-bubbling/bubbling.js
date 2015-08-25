b3979dd2689e1c0a80802f8da52a01c27193d5fe
Pan W <pan.wangperawong@generalassemb.ly>
2015-08-25 15:14:43 -0400

    updated class-notes with examples from today

--- u2-the-browser/d12-rev-up/class-notes/event-bubbling/bubbling.js
+++ u2-the-browser/d12-rev-up/class-notes/event-bubbling/bubbling.js
@@ -1,23 +1,18 @@
 $(function() {
-  $('body').click(function(event) {
-    alert('Bubbled up to <body>');
+  $('#level3').click(function(event) {
+    alert('Clicked on ' + $(event.currentTarget).attr('id'));
   });
 
-  $('div#level1').click(function(event) {
-    alert('Bubbled on level1')
+  $('#level2').click(function(event) {
+    event.stopPropagation();
+    alert('Clicked on ' + $(event.currentTarget).attr('id'));
   });
 
-  $('p#level2').click(function(event) {
-    alert('Bubbled on level2')
+  $('#level1').click(function(event) {
+    alert('Clicked on ' + $(event.currentTarget).attr('id'));
   });
 
-  $('span#level3').click(function(event) {
-    highlight(event.currentTarget);
+  $('body').click(function(event){
+    alert('Clicked on <body>');
   });
-
-  var highlight = function(element) {
-    $(element).css('background-color', 'yellow');
-    alert($(element).attr('id'));
-    $(element).css('background-color', '');
-  }
 });
