904b0b2acdb842442826770a0a8040d14dd4386b
Pan W <pan.wangperawong@generalassemb.ly>
2015-08-25 09:33:16 -0400

    added bullseye activity lesson

--- u2-the-browser/d12-rev-up/class-notes/bullseye/bullseye.js
+++ u2-the-browser/d12-rev-up/class-notes/bullseye/bullseye.js
@@ -1,3 +1,12 @@
+// ** TASK ** //
+// You are an Archer that is extremely precise and never miss.
+// Not even in the bubbly distorted world of JavaScript.
+
+// Add click event handlers to each bullseye ring that will alert when you hit
+// your desired target on the bullseye. Since you are in JavaScript
+// world your shot could bubble to other rings in the bullseye. Remember you
+// have a reputation to uphold, so make sure your shots do not bubble to other rings.
+
 $(function() {
-  // Add code
+  // Fire away
 });
