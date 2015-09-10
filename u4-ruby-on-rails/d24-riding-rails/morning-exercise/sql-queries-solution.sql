91a916ca35d13c64b0d8fb089abd5bafa6b34396
Bobby King <king.robertjoseph@gmail.com>
2015-09-10 12:08:29 -0400

    Adding Solutions to morning exercise

---
+++ u4-ruby-on-rails/d24-riding-rails/morning-exercise/sql-queries-solution.sql
@@ -0,0 +1,13 @@
+-- All Columns for all players from the New York Knicks (NYK)
+
+-- All Columns for all players from the Indiana Pacers (IND) who are under 26 years old
+
+-- All columns for all players, ordered from least points scored to most points scored 
+
+-- Name and points per game (points/games), for the players with the top 20 points per game
+
+-- The average age for all players 
+
+-- The average age for all players on the Oklahoma City Thunder (OKC)
+
+-- The average age for all of the players who've played more than 40 games
