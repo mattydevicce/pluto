d13ee187f2c453673d6b90daa126b2c6cc0cc5b6
Matt Morrisey <mattydevicce@gmail.com>
2015-09-10 09:11:02 -0400

    Finished homework

--- u4-ruby-on-rails/d23-sea-quail/student/ruby_powered_shopping_spree_hw/ruby_powered_shopping_spree.rb
+++ u4-ruby-on-rails/d23-sea-quail/student/ruby_powered_shopping_spree_hw/ruby_powered_shopping_spree.rb
@@ -26,3 +26,24 @@ items = [
   "Practice Perfect"
 ]
 
+$pg_conn = PG.connect({:dbname => 'shopping_db', :host => 'localhost'})
+
+insert_statement = $pg_conn.exec_params('INSERT INTO receipts (store,item,number_of_items,price,buy_date) VALUES ($1, $2, $3, $4, $5)', [stores[rand(stores.length)],items[rand(items.length)],quantities[rand(quantities.length)],prices[rand(stores.length)],Time.now])
+
+
+def ur(field, ut, locater, locaterset)
+  pans_jersey = $pg_conn.exec_params("UPDATE receipts SET #{field} = $1 WHERE #{locater} = $2", [ut, locaterset])
+end
+  
+ur('item', 'Giants Jersey', 'item', '49ers Jersey')
+
+ur('price', '350.00', 'item', 'PS4')
+
+def discard_item
+  length_of_receipts = $pg_conn.exec("SELECT * FROM receipts").count
+  $pg_conn.exec_params("DELETE FROM receipts WHERE id = $1", [rand(length_of_receipts)])
+end
+
+8.times do
+  discard_item  
+end
