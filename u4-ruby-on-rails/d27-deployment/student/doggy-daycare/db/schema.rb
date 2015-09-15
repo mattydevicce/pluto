8dc2cb1d548262f579fadee6e12cbadde8e51c95
Matt Morrisey <mattydevicce@gmail.com>
2015-09-15 16:06:02 -0400

    updated doggy-care schema accidently

--- u4-ruby-on-rails/d27-deployment/student/doggy-daycare/db/schema.rb
+++ u4-ruby-on-rails/d27-deployment/student/doggy-daycare/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150915151230) do
+ActiveRecord::Schema.define(version: 20150915181935) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,13 @@ ActiveRecord::Schema.define(version: 20150915151230) do
     t.string   "breed"
     t.string   "color"
     t.boolean  "tagged"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "microposts", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
