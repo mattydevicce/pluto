2c813704b69a18a57f71b080c1be376ffaf52878
Jaden Carver <jaden.carver@gmail.com>
2015-09-24 15:42:56 -0400

    Creating Boroughs

--- u6-apis/d34-sql-two/instructor/nycbbq/db/schema.rb
+++ u6-apis/d34-sql-two/instructor/nycbbq/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150924173114) do
+ActiveRecord::Schema.define(version: 20150924194211) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,10 @@ ActiveRecord::Schema.define(version: 20150924173114) do
     t.string   "location"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "boroughs", force: :cascade do |t|
+    t.string "code"
   end
 
   create_table "parks", force: :cascade do |t|
