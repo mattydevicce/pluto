88575f515baf1c988580ea7f7e2068fff534f6af
Dennis Liaw <dennis.liaw@gmail.com>
2015-09-25 11:40:54 -0400

    adds oauth app

---
+++ u6-apis/d35-oauth/instructor/oauth_app/app.rb
@@ -0,0 +1,94 @@
+module App
+  class Server < Sinatra::Base
+
+    # these are private, and we don't want them on github
+    CLIENT_ID = ENV['GITHUB_CLIENT_ID']
+    CLIENT_SECRET = ENV['GITHUB_CLIENT_SECRET']
+
+    enable :sessions
+    set :session_secret, 'secret lovers'
+
+    configure :development do
+      require 'pry'
+      register Sinatra::Reloader
+    end
+
+    get '/' do
+      base_url = "https://github.com/login/oauth/authorize"
+      redirect_uri = "http://localhost:9292/oauth_callback"
+      state = SecureRandom.urlsafe_base64 # a random unguessable string
+
+      session[:auth_state] = state # saving this for checking later
+      query = "client_id=#{CLIENT_ID}&redirect_uri=#{redirect_uri}&state=#{state}"
+
+      @auth_url = "#{base_url}/?#{query}"
+      # binding.pry
+      erb :index
+    end
+
+    get '/oauth_callback' do
+      #  check if the response is ours
+      code  = params[:code]
+      state = params[:state]
+
+      if session[:auth_state] == state
+        # we're good, so lets query Github
+
+        access_token_url = "https://github.com/login/oauth/access_token"
+
+        data = {
+              client_id: CLIENT_ID,
+          client_secret: CLIENT_SECRET,
+                   code: code,
+          redirect_uri: "http://localhost:9292/oauth_callback"
+        }
+
+        headers = {
+          Accept: :json
+        }
+
+        github_response = RestClient.post( access_token_url, data, headers)
+
+        session[:access_token] = JSON.parse(github_response)["access_token"]
+
+        redirect '/logged_in'
+
+      else
+        "WE'VE BEEN TAMPERED WITH"
+      end
+
+
+    end#/oauth_callback
+
+    get '/logged_in' do
+      # auth established, lets get shit from github
+
+      headers = {
+        Accept: "application/vnd.github.v3+json",
+        Authorization: "token #{session[:access_token]}"
+      }
+
+      url = "https://api.github.com/user"
+
+      github_response = RestClient.get url, headers
+      # binding.pry
+
+      @user = JSON.parse github_response
+
+      erb :logged_in
+
+    end
+
+  end # Server
+end # App
+
+
+
+
+
+
+
+
+
+
+
