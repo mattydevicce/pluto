3588e215064c290482e788f7e513ab8ba5613fc2
Matt Morrisey <mattydevicce@gmail.com>
2015-10-16 11:26:06 -0400

    Learned how to implement a TCP connection using socket.io

--- u8-nodejs/d49-expressive-reaction/homework/index.js
+++ u8-nodejs/d49-expressive-reaction/homework/index.js
@@ -3,20 +3,21 @@ var mongo = require('mongodb');
 var ejs = require('ejs')
 var client = mongo.MongoClient;
 var app = express();
-
+app.set('view engine', 'ejs')
 
 client.connect('mongodb://localhost:27017/monsters', function(error, db) {
-	
-	var template = ejs.compile();
 
 	var query = db.collection('monsters').find()
+
 	var vampires = query.toArray(function(error, vampires) {
-		template(vampires)
+		
+		app.get('/', function(req, res) {
+			
+			res.render('show', {vampires: vampires});
+		
+		})
   })
 
-	app.get('/', function(req, res) {
-		res.rebder('show', {vampires: vampires});
-	})
 	
 })
 
--- u8-nodejs/d50-pushing-it/student/chat-app/app.js
+++ u8-nodejs/d50-pushing-it/student/chat-app/app.js
@@ -1,1 +1,23 @@
+var express = require('express');
+var morgan  = require('morgan');
+var app     = express();
 
+var server = app.listen(3000);
+var io     = require('socket.io')(server);
+
+app.use(morgan('combined'));
+
+app.use( express.static('public'));
+
+io.on( 'connection', function(socket) {
+
+	console.log('INCOMING CONNECTION');
+
+	socket.emit("hello", {message: 'hi from wdi', admin: 'matt'})
+	socket.on('CLICK', function(msg) {
+		console.log( msg.name + " said: " + msg.message );
+		io.emit('christmas', msg);
+	});
+
+
+}); // io
