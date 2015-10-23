f37ba1284d278c1c14e43076a73b006ad1d65334
Jaden Carver <jaden.carver@gmail.com>
2015-10-23 09:07:40 -0400

    Prompt

---
+++ u9-final-project/prompt.rb
@@ -0,0 +1,77 @@
+# Final Project
+
+![final countdown](http://i.imgur.com/VQv50cY.gif)
+
+It's time to go out and build something. As always, project week will be a
+*sprint*, and doesn't end on the final day. Keep that in mind, and keep your
+MVP focused!
+
+You should make something:
+
+1. that you've always wanted to make, or
+2. something that you feel will reinforce and demonstrate the skills you've learned throughout WDI
+
+Either way, think big, but also describe a modest set of features for your MVP. You should be challenging yourself to make something that pushes your boundaries and takes you out of your comfort zone. That being said, your MVP should be scaffolded appropriately so you have acheivable goals to work towards. If you do this, you will have a great MVP to show at the end of this sprint, and a project that you want to continue to work on and build up after the sprint is over!
+
+## Project Objectives
+
+- write code you want people to read, code that you're proud of and want to show off
+- build something nice and small (not a sprawling mess, but a real **MVP**)
+- build something with potential to grow (so that you can continue adding features to it after the course)
+- manage your time and emotions well (remember: this is a test-run for your life post-course)
+
+## Requirements
+
+#####Your application must:
+   - have a public GitHub repo with comprehensive README documentation (see below).
+   - follow a style (you can use [Rubocop](https://github.com/bbatsov/rubocop) and [jsHint](https://github.com/jshint/jshint) to check yourself before you wreck yourself with unstylish code)
+   - follow the principles of good software:
+     - semantic naming
+     - small procedures and classes
+     - a DRY structure (no-repeated code)
+     - readable and commented (as necessary)
+   - be deployed
+
+#####You, as an indidivual must:
+   - present your work and describe why you created it, eg:
+     - *"It's an area or project of interest to me - this is a proof of concept."*
+     - *"I decided to build this project because I learned about x, y, z in this course, and found those things interesting."*
+
+## Timeline
+
+### Throughout the Sprint - One-on-Ones
+You must have at least one, and no more than two 45 minute one-on-ones with an instructor. [See this speadsheet to sign up for time slots](https://docs.google.com/spreadsheets/d/1TKcGEfDzgxsdGE7X-6Pc7bzCruvTK-EH8lzWoG7acZo/edit#gid=0).
+
+### Monday, October 26th (beginning of day) - Deliverables Due
+
+- A single paragraph description of what you want your project to be, and what technologies you plan to use
+- A Trello board (that all of the instructors have been added to) that lists features as user stories (eg: *"A user can log in." "A user can use the search feature without logging in."*)
+- User stories organized into lists, eg:
+  - **Current** (what you're currently working on)
+  - **Sprint** (what you hope to have finished in this sprint, ie your MVP)
+  - **Backlog** (your wishlist)
+  - (or any other organizational schema that is clear and useful)
+- ERD (if applicable)
+- Wireframes
+
+You will have a 15 minute check in on Monday with an instructor to go over these deliverables. **This meeting is not for technical help.** [See this schedule to know when and with whom you are meeting](check-in-schedules.md). Don't miss it!
+
+### Thursday, October 29th - Individul Retro
+
+You will have a 15 minute one-on-one retro with an instructor. The purpose of this appointment is to check in and reflect on your process: what's going well, what isn't, and what strategies will help carry you through the rest of the sprint. **This meeting is not for technical help.** [See this schedule to know when and with whom you're meeting](check-in-schedules.md). Don't miss it!
+
+### Monday, November 2nd (9 AM) - Deliverables Due
+You should have:
+
+- An application, with its code and documentation on GitHub that includes:
+  - a Trello board with the state of the features
+  - a link to the deployed app
+  - a short description
+  - a list of technologies used (with appropriate links)
+  - artifacts:
+    - wireframes
+    - ERD (if applicable)
+- A 5 minute Lightning talk during which you:
+  - demo your application
+  - show a little bit of code
+  - tell the class what you are most proud of, and what feature you are going to implement next. After you've finished your presentation, you will have a little time to answer questions from the class about your work.
